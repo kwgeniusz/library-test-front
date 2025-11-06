62c064d250e5aac08189e182f99a71e0e1037ca6
Manuel Castro <manuelcastrophp@gmail.com>
2025-11-06 00:10:59 -0400

    BaseURL modification

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://127.0.0.1:8001/api',
+  baseURL: process.env.VUE_APP_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json'
   }
